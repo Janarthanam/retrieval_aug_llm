a85fec0ffd42d0e43f19165b54cf589926f7a529
Janar Ramalingam <janar.ramalingam@thoughtspot.com>
2023-07-02 21:39:10 +0530

    Changes to make search work better
    1. Move back to llm qa "stuff" mode as map_reduce is clueless
    2. Created more metadata around documents.
       * allow create different collection spaces for db.
         /v1/admin/db -d "name={collection}"
       * within collection allow file names so that answers can be annotated
         {content: {answer :"I don't know", files:["a1","a2"]}}
         would mean openAI doesn't know about the question even though
         embedding returned two files.
    
    Lot more work to do

--- api/routes/admin.py
+++ api/routes/admin.py
@@ -1,13 +1,15 @@
 #This is to init the vector store
 
+from typing import Annotated
+
 from qdrant_client.models import VectorParams, Distance
-from fastapi import APIRouter
+from fastapi import APIRouter, Body
 from db import vector_store
 
 router = APIRouter()
 
 @router.put("/admin/v1/db")
-async def recreate_collection(name: str = "test"):
+async def recreate_collection(name: Annotated[str, Body(embed=True)]):
     print(f"creating collection {name} in db")
     return vector_store.client.recreate_collection(collection_name=name, 
                                             vectors_config=VectorParams(size=1536, distance=Distance.COSINE))
